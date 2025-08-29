4f16e9915907eaee41841dd8ffb68320cc293f69
Vitalii Kaplan <vitalii.v.kaplan@gmail.com>
2025-08-29 10:06:14 +0300

    Run script

---
+++ k2p.sh
@@ -0,0 +1,2 @@
+python k2p.py ../../harbour/KNIME/KNIME_project_csv_sample --out ../../../Downloads/K2P/ --workbook ipynb
+#python k2p.py ../../harbour/KNIME/ISU_Master --out ../../../Downloads/K2P/ --workbook ipynb
